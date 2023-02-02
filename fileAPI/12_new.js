f10a5d9285e5274648816984f4ae86d5b07148bb
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-02 14:53:02 +0800

    從檔案總管將檔案拖到瀏覽器 - 文字檔

---
+++ fileAPI/12_new.js
@@ -0,0 +1,24 @@
+function doFirst(){
+  //先跟畫面產生關聯，再建事件聆聽功能
+  document.getElementById('dropzone').ondragover = dragOver
+  document.getElementById('dropzone').ondrop = dropped
+}
+function dragOver(e){
+  e.preventDefault()
+  
+}
+function dropped(e){
+  e.preventDefault()
+
+  //指定某個檔案
+  let file = e.dataTransfer.files[0] 
+  //顯示拖放檔案的名稱
+  document.getElementById('fileName').innerText = file.name 
+  //顯示拖放檔案的文字內容
+  let readFile = new FileReader() //fileReader型態
+  readFile.readAsText(file)  //以純文字格式回傳結果
+  readFile.addEventListener('load',function(){
+    document.getElementById('fileContent').value = readFile.result
+  })
+}
+window.addEventListener('load',doFirst)
