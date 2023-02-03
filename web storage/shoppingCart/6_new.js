223146842126a33a33d2b32f0807313cdc7af314
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-03 14:41:01 +0800

    delete item in the cart

--- web storage/shoppingCart/6_new.js
+++ web storage/shoppingCart/6_new.js
@@ -11,11 +11,15 @@ function doFirst(){
   newDiv.appendChild(newTable)
   document.getElementById('cartList').appendChild(newDiv)
 
+  //total金額
+  total = 0
   for(let i = 0; i < items.length; i++){
     let itemInfo = storage.getItem(items[i])
+    total += parseInt(itemInfo.split('|')[2])
     
     createCartList(items[i],itemInfo)
   }
+  document.getElementById('total').innerText = total
 }
 
   function createCartList(itemId, itemValue){
@@ -65,11 +69,52 @@ function doFirst(){
 
     let pPrice = document.createElement('p')
     pPrice.innerText = itemPrice
-    
+
     tdPrice.appendChild(pPrice)
     trItemList.appendChild(tdPrice)
 
     // 建立商品數量 -- 第四個 td
+    let tdItemCount = document.createElement('td')
+    tdItemCount.style.width = `60px`
+
+    let pItemCount = document.createElement('p')
+    
+    let inputItemCount = document.createElement('input')
+    inputItemCount.type = `number`
+    inputItemCount.value = 1
+    inputItemCount.min = 1  //最少值為1，無法再向下遞減
+    inputItemCount.addEventListener('',changeItemCount)
+    
+    //input放進p裡
+    pItemCount.appendChild(inputItemCount)
+    //p放進td裡
+    tdItemCount.appendChild(pItemCount)
+    //td放進tr裡
+    trItemList.appendChild(tdItemCount)
   }
-function deleteItem(){}  
+//刪除購物車裡的商品
+function deleteItem(e){
+  let itemId = e.target.parentNode.id //e.target.parentNode -> button
+  //alert(itemId)
+
+  //1.先扣總金額
+  let itemValue = storage.getItem(itemId)
+  total -= parseInt(itemValue.split('|')[2])
+  document.getElementById('total').innerText = total
+
+  //2.清除 storage
+  storage.removeItem(itemId)
+  storage['addItemList'] = storage['addItemList'].replace(`${itemId}, `, ``) //空字串取代
+
+  //3. 刪除該筆<tr></tr>
+  //e.target => button本身 
+  //parentNode.parentNode.parentNode 順序為: td->tr->table
+
+  //寫法一
+  // e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode)
+  //寫法二
+  newTable.removeChild(e.target.parentNode.parentNode)
+
+}  
+function changeItemCount(){}
 window.addEventListener('load', doFirst);
